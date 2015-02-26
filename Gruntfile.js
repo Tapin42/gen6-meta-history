880639797520c8c265a75061c240946297b37c54
Joe Navratil <navratil@gmail.com>
2015-02-26 08:08:47 -0800

    Initial versions of the retriever, taken mostly from gen6-damage-calc

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        globalLinkDownload: {
+            rawDir: '<%= globalLink.rawDir %>',
+            request: {
+                url: 'http://3ds.pokemon-gl.com/frontendApi/gbu/getSeasonPokemonDetail',
+                headers: {
+                    Referer: 'http://3ds.pokemon-gl.com/battle/oras/'
+                },
+                initialParams: {
+                    languageId: 2,
+                    seasonId: 108,
+                    battleType: 2,
+                    timezone: 'EST',
+                    pokemonId: '1-0',
+                    displayNumberWaza: 10,
+                    displayNumberTokusei: 3,
+                    displayNumberSeikaku: 10,
+                    displayNumberItem: 10,
+                    displayNumberLevel: 10,
+                    displayNumberPokemonIn: 10,
+                    displayNumberPokemonDown: 10,
+                    displayNumberPokemonDownWaza: 10
+                }
+            }
+        },
+        globalLink: {
+            rawDir: process.cwd() + '/rawData'
+        }
+    });
+
+    grunt.loadTasks('grunt');
+
+    grunt.registerTask('default', ['globalLink']);
+    grunt.registerTask('all', ['globalLinkDownload', 'globalLink']);
+};
